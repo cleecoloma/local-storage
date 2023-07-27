561597d06391518126d1f358357965b74606a622
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-27 14:28:04 -0700

    completed css and chart

--- js/app.js
+++ js/app.js
@@ -38,22 +38,14 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
-  let saveData = JSON.stringify(this.allProducts);
-  localStorage.setItem('products', saveData);
-  console.log(saveData);
 };
 
 AppState.prototype.loadItems = function () {
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
-  let chartData = JSON.parse(localStorage.getItem('products'));
-  // console.log(chartData);
 
-  if (!chartData) {
-    this.instantiateProducts();
-  } else {
-    this.allProducts = chartData;
-  }
+  this.instantiateProducts();
 };
+
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
